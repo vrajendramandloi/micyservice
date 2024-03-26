26a1ddfea9576d8c8fdbc80c878304ad819bce1e
vikkymandloi <imvrajendra@gmail.com>
2024-03-26 23:22:59 +0530

    fix for mail and zip resource files

--- src/main/java/com/uni/micy/service/controller/DownloadInitSetup.java
+++ src/main/java/com/uni/micy/service/controller/DownloadInitSetup.java
@@ -1,5 +1,9 @@
 package com.uni.micy.service.controller;
 
+import com.uni.micy.service.util.AppUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -9,9 +13,55 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @RestController
+@Slf4j
 public class DownloadInitSetup {
-	
+    private File zipResourceFile = null;
+    private Logger log = LoggerFactory.getLogger(DownloadInitSetup.class);
+
+    @PostConstruct
+    public void createZipFile() {
+        Resource resource = null;
+        try {
+            resource = new ClassPathResource("json/listify.json");
+            File resourceFile = resource.getFile();
+            String resourceFilePath = resourceFile.getAbsolutePath();
+            resourceFilePath = resourceFilePath.replace("listify.json","");
+            String jarPath = getClass()
+                    .getProtectionDomain()
+                    .getCodeSource()
+                    .getLocation()
+                    .toURI()
+                    .getPath();
+
+            log.info("Jar Path : "+ jarPath);
+            if(jarPath.contains(".jar")) {
+                jarPath = jarPath.substring(0, jarPath.lastIndexOf("/"));
+            }
+            log.info("Jar Path source : "+ jarPath);
+            Path path = Paths.get(resourceFilePath);
+            try (Stream<Path> paths = Files.walk(path)) {
+                List<Path> jsonPathList = paths.filter(x -> x.getFileName().toString().endsWith(".json"))
+                        .collect(Collectors.toList());
+                log.info(jsonPathList.toString());
+                zipResourceFile = AppUtils.addFilesToZip(jarPath, jsonPathList);
+                log.info("zipFile "+zipResourceFile.getAbsolutePath());
+            }
+        } catch (Exception e) {
+            log.error("Exception while constructing zip of all Json ",e);
+        }
+    }
+
 	@GetMapping("/initsetup/download")
     public ResponseEntity<?> downloadFile() {
         Resource resource = null;
--- src/main/java/com/uni/micy/service/util/AppUtils.java
+++ src/main/java/com/uni/micy/service/util/AppUtils.java
@@ -1,15 +1,38 @@
 package com.uni.micy.service.util;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
 public class AppUtils {
-	public static boolean isEmpty(String str) {
-		if(str != null && str.equalsIgnoreCase("null")) return false;
-		return (str != null && str.trim().length() != 0);
-	}
-	public static synchronized String getCurrency(String country) {
-		String currency = "INR";
-		if(!"INDIA".equalsIgnoreCase(country)) {
-			currency = "USD";
+    public static boolean isEmpty(String str) {
+        if (str != null && str.equalsIgnoreCase("null")) return false;
+        return (str != null && str.trim().length() != 0);
+    }
+
+    public static synchronized String getCurrency(String country) {
+        String currency = "INR";
+        if (!"INDIA".equalsIgnoreCase(country)) {
+            currency = "USD";
+        }
+        return currency;
+    }
+
+    public static File addFilesToZip(String source, List<Path> filesPathList) throws IOException {
+		String zipPath = source+"/micyConfigFile.zip";
+
+		try (ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(zipPath))) {
+			for (Path filePath : filesPathList) {
+				File fileToZip = filePath.toFile();
+				zipOut.putNextEntry(new ZipEntry(fileToZip.getName()));
+				Files.copy(fileToZip.toPath(), zipOut);
+			}
 		}
-		return currency;
-	}
+		return new File(zipPath);
+    }
 }
